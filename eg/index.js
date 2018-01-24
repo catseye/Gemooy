09b51a8a2d2643bd6e857bb616d0bff80515fd02
Chris Pressey <cpressey@catseye.tc>
2018-01-24 14:38:12 +0000

    Load example programs from JSONP instead of embed in HTML elements.

---
+++ eg/index.js
@@ -0,0 +1,14 @@
+examplePrograms = [
+    [
+        "grow-indefinitely.gemooy", 
+        " @@ %\n@  $\n@   #\n     #\n      @# @\n          @\n          @\n     @   @\n"
+    ], 
+    [
+        "toggle-column.gemooy", 
+        "%   @@   @@\n#  @  $    @\n       @\n#      #\n#\n       @\n#     # #\n#    #\n    @     @\n    @     @@\n#  @ @    @\n@   @   @\n"
+    ], 
+    [
+        "turn-around.gemooy", 
+        "%           @\n\n\n @@      @@\n@       @  @\n@          @\n @   $    @\n      #\n       #\n        @  # @#  @\n              @   @\n              @   #\n           @ @\n\n            @@             @@\n           @              @  @\n           @                 @\n            @               @\n                 #\n                  @  #@#    @\n                 #     @     @ \n                #         @  @\n               @           @@\n               @       @\n                @     @\n"
+    ]
+];
